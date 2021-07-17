96e47405d7252692a70fe3ee2470ddafedcbc67d
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-07-17 22:15:02 +0200

    hash does not work

--- ppm/rgb_color.h
+++ ppm/rgb_color.h
@@ -5,14 +5,21 @@
 #ifndef PPM_EDITOR_PPM_RGB_COLOR_H_
 #define PPM_EDITOR_PPM_RGB_COLOR_H_
 
+#include <ostream>
 struct RGBColor {
 
-  RGBColor(unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b) {}
+  RGBColor(unsigned char r, unsigned char g, unsigned char b)
+      : r(r), g(g), b(b) {}
+  RGBColor(int hash) : r(hash & 16711680), g(hash & 65280), b(hash & 255) {}
   unsigned char GetR() const { return r; }
   unsigned char GetG() const { return g; }
   unsigned char GetB() const { return b; }
-  RGBColor(): r(0), g(0), b(0) {}
-
+  RGBColor() : r(0), g(0), b(0) {}
+  friend std::ostream &operator<<(std::ostream &os, const RGBColor &color) {
+    os << "r: " << (int)color.r << " g: " << (int)color.g
+       << " b: " << (int)color.b;
+    return os;
+  }
   bool operator==(const RGBColor &rhs) const {
     return r == rhs.r && g == rhs.g && b == rhs.b;
   }
